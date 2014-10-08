20c879a4c14efb341ba11d36277eae076e64dc6b
Vinai Kopp <vinai@netzarbeiter.com>
2014-10-08 17:50:22 +0200

    Issue #1: Add test to check if a backend can be swapped via the repository

---
+++ tests/Integration/Suites/ClassRelationsTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Brera\Lib\Queue\Tests\Integration;
+
+
+
+use Brera\Lib\Queue\Factory\FileQueueFactory;
+use Brera\Lib\Queue\Backend\File\FileConfig;
+
+class ClassRelationsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var FileQueueFactory
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new FileQueueFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function theQueueBackendInUseShouldBeExchangeableViaTheRegistryInstance()
+    {
+        $messageA = 'Message A';
+        $messageB = 'Message B';
+        
+        $repositoryA = $this->factory->getNewRepository();
+        $repositoryB = $this->factory->getNewRepository();
+
+        $this->switchToQueue($repositoryA);
+        $this->configureQueue('A');
+        $this->sendMessageOnActiveQueue($messageA);
+
+        $this->switchToQueue($repositoryB);
+        $this->configureQueue('B');
+        $this->sendMessageOnActiveQueue($messageB);
+
+        $messagePayload = $this->consumeMessageFromActiveQueue();
+        $this->assertEquals($messageB, $messagePayload);
+        
+        $this->switchToQueue($repositoryA);
+        
+        $messagePayload = $this->consumeMessageFromActiveQueue();
+        $this->assertEquals($messageA, $messagePayload);
+    }
+
+    private function configureQueue($identifier)
+    {
+        /** @var FileConfig $config */
+        $config = $this->factory->getRegisteredBackendConfigInstance();
+        $config->setStorageRootDir(sys_get_temp_dir() . DIRECTORY_SEPARATOR . $identifier);
+        $config->getKeepProcessedMessages(false);
+    }
+
+    private function sendMessageOnActiveQueue($payload)
+    {
+        $producer = $this->factory->getProducerQueue();
+        $producer->sendMessageByChannel('test-channel', $payload);
+    }
+
+    private function consumeMessageFromActiveQueue()
+    {
+        $consumer = $this->factory->getConsumerQueue();
+        $message = $consumer->receiveMessageFromChannel('test-channel');
+        $messagePayload = $consumer->getMessagePayload($message);
+        return $messagePayload;
+    }
+
+    private function switchToQueue($repositoryA)
+    {
+        $this->factory->setRepository($repositoryA);
+    }
+} 
