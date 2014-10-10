2d3207f5b03e421e462b95dc61e7b3d27c56471b
Vinai Kopp <vinai@netzarbeiter.com>
2014-10-10 11:48:02 +0200

    Issue #1: Add integration test to check if two separete queues can be used without resetting the factory repository between usage

--- tests/Integration/Suites/ClassRelationsTest.php
+++ tests/Integration/Suites/ClassRelationsTest.php
@@ -46,6 +46,43 @@ class ClassRelationsTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($messageA, $messagePayload);
     }
 
+    /**
+     * @test
+     * @coversNothing
+     */
+    public function itShouldAllowUsingTwoQueuesInParallelWithoutAlwaysSwitchingTheFactoryRepository()
+    {
+        $repositoryA = $this->factory->getNewRepository();
+        $repositoryB = $this->factory->getNewRepository();
+
+        $this->switchToQueue($repositoryA);
+        $this->configureQueue('A');
+        $producerQueueA = $this->factory->getProducerQueue();
+        $consumerQueueA = $this->factory->getConsumerQueue();
+
+        $this->switchToQueue($repositoryB);
+        $this->configureQueue('B');
+        $producerQueueB = $this->factory->getProducerQueue();
+        $consumerQueueB = $this->factory->getConsumerQueue();
+        
+        $producerQueueA->sendMessageByChannel('test-channel', 'A1');
+        $message = $consumerQueueA->receiveMessageFromChannel('test-channel');
+        $this->assertEquals('A1', $consumerQueueA->getMessagePayload($message));
+        
+        $producerQueueB->sendMessageByChannel('test-channel', 'B1');
+        $message = $consumerQueueB->receiveMessageFromChannel('test-channel');
+        $this->assertEquals('B1', $consumerQueueB->getMessagePayload($message));
+
+        $producerQueueB->sendMessageByChannel('test-channel', 'B2');
+        $producerQueueA->sendMessageByChannel('test-channel', 'A2');
+        
+        $message = $consumerQueueB->receiveMessageFromChannel('test-channel');
+        $this->assertEquals('B2', $consumerQueueB->getMessagePayload($message));
+        
+        $message = $consumerQueueA->receiveMessageFromChannel('test-channel');
+        $this->assertEquals('A2', $consumerQueueA->getMessagePayload($message));
+    }
+
     private function configureQueue($identifier)
     {
         /** @var FileConfig $config */
